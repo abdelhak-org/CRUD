6b7e2745460aeda0c24592b1c911235a4b81bcb8
abdelhak-org <abdelhak.org@gmail.com>
2023-12-23 07:15:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+     <h3 className='p-2 mt-4 text-primary font-mono'>CRUD SYSTEM</h3>
+     <Container className='border p-4 rounded-4  '>
+      
+     </Container>
+    </div>
+  );
+}
+
+export default App;
